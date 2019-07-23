312795a5f054f3f3ac9c7599e284c1be1833d269
David Bucklin <david.bucklin@dcr.virginia.gov>
2019-07-23 11:29:15 -0400

    init directories, files

--- part2.R
+++ part2.R
@@ -16,18 +16,20 @@ rules$desc[5] <- "y6"
 
 # eq = function(x){x*x}
 rules <- rules[!duplicated(rules$desc), ]
-rules <- rules[order(rules$desc),]
-png('rplot.png', width = 6, height = 4, units = 'in', res = 600)
+rules <- rules[order(rules$desc), ]
+png("rplot.png", width = 6, height = 4, units = "in", res = 600)
 
 
-#dev.new(width=24,height=16,noRStudioGD = TRUE)
-val <-  function(x,a,b,scalar,weight) { (1/(1+(exp(((x/scalar)-a)*b))))*weight }
-plotcol <- brewer.pal(6,"Accent")
-plot(1,type='n',xlim=c(0,2000),ylim=c(0,max(rules$weight)),xlab='Distance', ylab='Weight',bty="n")
-for (s in 1:nrow(rules)) {
-  lines(val(1:2000,a=rules$a[s],b=rules$b[s],scalar=rules$scalar[s],weight=rules$weight[s]), type='l',col=plotcol[s], lwd=2 )
+# dev.new(width=24,height=16,noRStudioGD = TRUE)
+val <- function(x, a, b, scalar, weight) {
+    (1/(1 + (exp(((x/scalar) - a) * b)))) * weight
 }
-legend("topright",legend=rules$desc, col=plotcol, lty=c(1,1),cex=0.8,bty="n")
+plotcol <- brewer.pal(6, "Accent")
+plot(1, type = "n", xlim = c(0, 2000), ylim = c(0, max(rules$weight)), xlab = "Distance", ylab = "Weight", bty = "n")
+for (s in 1:nrow(rules)) {
+    lines(val(1:2000, a = rules$a[s], b = rules$b[s], scalar = rules$scalar[s], weight = rules$weight[s]), type = "l", col = plotcol[s], lwd = 2)
+}
+legend("topright", legend = rules$desc, col = plotcol, lty = c(1, 1), cex = 0.8, bty = "n")
 
 
 
